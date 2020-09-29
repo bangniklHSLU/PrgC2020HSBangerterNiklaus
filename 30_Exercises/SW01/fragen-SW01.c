494b84cf8deb41bb2a2bf29f27b7f470643cd4d5
niklaus <niklaus.bangerter@stud.hslu.ch>
2020-09-29 15:55:30 +0200

    fragen SW01 beantwortet

--- 30_Exercises/SW01/fragen-SW01.c
+++ 30_Exercises/SW01/fragen-SW01.c
@@ -1,6 +1,13 @@
 Werden neue Files aus Terminal mit "touch name.format", oder mit anderem Befehl erstellt?
+touch: erstellen
+gedit: erstellen und editieren
 Wie wird der Texteditor aus der Terminal geöffnet?
+gedit
 Was macht der Befehl "pico name.format"?
+?
 Werden die verschiedenen Einzüge im Texteditor automatisch erstellt (wie im Bsp-C-Programm)?
+nein, manuell
 Wann erkennt System ".c"-File als solches (Logo ändert im Verzeichnis)?
+?, aber nicht wichtig
 Weshalb muss mit "git config --global user.email" eine E-Mail-Adresse angegeben werden vor "git commit -m "..." "?
+?, aber nicht wichtig
