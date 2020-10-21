04f32e0d020881dabca59b292fbc3583e683dadf
niklaus <niklaus.bangerter@stud.hslu.ch>
2020-10-21 16:45:07 +0200

    Erster Code

---
+++ 30_Exercises/SW06/readTable.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main( int argc, char* argv[] )
+{
+	long long timeStamp = 0;
+	int pressureVal = 0;
+	char systemState = 0;
+	char alarmState = 0;
+	
+	FILE *pRead = fopen("pressureSpike.bin", "rb");
+	if( pRead != NULL)
+	{
+		while((timeStamp = getchar()) != EOF)	
+		{
+			fread(&timeStamp, sizeof(long long), 1, pRead);
+			printf("timeStamp is: %lld\n", timeStamp);
+			fread(&pressureVal, sizeof(int), 1, pRead);
+			printf("pressureVal is: %d\n", pressureVal);
+			
+			fread(&systemState, sizeof(char), 1, pRead);
+			printf("systemState is: %d\n", systemState);
+			
+			fread(&alarmState, sizeof(char), 1, pRead);
+			printf("alarmState is: %d\n", alarmState);
+		}
+	}
+	
+	return 0;
+}
